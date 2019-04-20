0bf5c5f859f5e2d0dda968cff8f13995b0065e51
arunsah <arunkumar626@gmail.com>
2019-04-21 00:45:23 +0530

    logger logic working

--- src/logger/log.go
+++ src/logger/log.go
@@ -1,10 +1,9 @@
 package logger
 
 import (
-	"fmt"
 	"log"
-	SysLog "log"
 	"os"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -24,65 +23,47 @@ const ( // iota is reset to 0
 
 // Info will use by different goroutines while logging to file or to screen
 type Info struct {
-	LogTime     time.Time
-	Type        int // WARN | INFO | ERROR | DEBUG
-	FileName    string
-	Method      string
-	BlockNumber string
-	Message     string
-	Error       error
+	LogTime   time.Time
+	Type      int // WARN | INFO | ERROR | DEBUG
+	Package   string
+	Method    string
+	ErrorCode string
+	Message   string
+	Error     error
 }
 
-// Log is dummy struct
-type Log struct {
-	fileHandle os.File
+func (info *Info) String() string {
+	txt := ""
+	txt += info.LogTime.String() + "\t : "
+	txt += strconv.Itoa(info.Type) + " : "
+	txt += info.Package + "."
+	txt += info.Method + " # "
+	txt += info.ErrorCode + " : "
+	txt += info.Message
+	if info.Error != nil {
+		txt += " : " + info.Error.Error()
+	}
+	txt += ";\n"
+	return txt
 }
 
-var instance *Log
-var logOnce sync.Once
-
-// Logger log
-func Logger() *Log {
-	logOnce.Do(func() {
-		instance = &Log{}
-	})
-	return instance
-}
-
-func AppendFile() {
-	file, err := os.OpenFile("test.txt", os.O_WRONLY|os.O_APPEND, 0644)
+// Logger logs
+func Logger(filename string, infoCh <-chan Info, shutdownCh <-chan bool, wg *sync.WaitGroup) {
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		log.Fatalf("failed opening file: %s", err)
 	}
 	defer file.Close()
 
-	len, err := file.WriteString(" The Go language was conceived in September 2007 by Robert Griesemer, Rob Pike, and Ken Thompson at Google.")
-	if err != nil {
-		log.Fatalf("failed writing to file: %s", err)
+loop:
+	for {
+		select {
+		case info := <-infoCh:
+			file.WriteString(info.String())
+		case shutdown := <-shutdownCh:
+			log.Println("closing logger", shutdown)
+			break loop
+		}
 	}
-	fmt.Printf("\nLength: %d bytes", len)
-	fmt.Printf("\nFile Name: %s", file.Name())
-}
-
-func (log *Log) Error() {
-
-}
-
-// SetFile open a file in append mode and assign it to fileHandle
-func (log *Log) SetFile(fileName string) {
-	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		SysLog.Fatalf("failed opening file: %s", err)
-	}
-	log.fileHandle = *file
-}
-
-// Close closes the log file
-func (log *Log) Close() {
-	log.fileHandle.Close()
-}
-
-// TestLogger test
-func TestLogger() {
-	//Logger().
+	wg.Done()
 }
--- src/main.go
+++ src/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"os"
+	"strings"
+	"sync"
 	"time"
 
 	Logger "./logger"
@@ -20,7 +25,25 @@ func main() {
 	ll := &Logger.Info{Type: Logger.WARN, Message: "Error ocurred"}
 	fmt.Println(ll)
 	fmt.Println(time.Now())
-	Logger.Log.
+
+	infoCh := make(chan Logger.Info, 1000)
+	shutdownCh := make(chan bool)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go Logger.Logger("txt1.txt", infoCh, shutdownCh, &wg)
+	//infoCh <- *ll
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		txt, _ := reader.ReadString('\n')
+		txt = strings.TrimSpace(txt)
+		if strings.ToLower(txt) == "shutdown" {
+			shutdownCh <- true
+			break
+		}
+		infoCh <- Logger.Info{LogTime: time.Now(), Type: Logger.INFO,
+			Package: "main", Method: "main()", ErrorCode: "0", Message: txt, Error: errors.New("no-error")}
+		//fmt.Println(txt)
+	}
 
 }
 
